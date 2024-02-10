4318bbcc806f2c2d843b22068a21688d1e972de1
Atharva-521 <136982860+Atharva-521@users.noreply.github.com>
2024-02-10 19:19:28 +0530

    Sign and Login Route Handler Added

--- Controllers/Auth.js
+++ Controllers/Auth.js
@@ -1,6 +1,11 @@
 const User = require("../Models/User");
 const OTP = require("../Models/OTP");
+const Profile = require("../Models/profile")
 const otpGenerator = require('otp-generator');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken")
+require("dotenv").config()
+
 
 //OTP Creation/generation
 exports.sendOTP = async (req,res) => {
@@ -57,8 +62,141 @@ exports.sendOTP = async (req,res) => {
         })
     }
 }
-//signUp
+//signUp 
+exports.signUp = async(req, res) => {
+    try{
+        // fetch data from req
+        const {firstName,lastName,email,phoneNumber,password,confirmPassword,otp} = req.body;
+        //validate data
+        if(!firstName || !lastName || !email || !phoneNumber || !password || !confirmPassword || !otp){
+            return res.status(401).json({
+                success: false,
+                message: "All fields are required"
+            })}
+        //user exists
+        const user = await User.findOne({email});
 
+        if(user){
+            return res.status(401).json({
+                success: false,
+                message: "User Already Registered, Please Log In To Proceed"
+            })
+        }
+        //otp match
+        const recentOTP = await OTP.find({email}).sort({createdAt: -1}).limit(1);
+
+        if(recentOTP !== otp){
+            return res.status(403).json({
+                success: false,
+                message: "OTP Doesn't Match, Try Again"
+            })
+        }
+        //password validate
+        if(password !== confirmPassword){
+            return res.status(403).json({
+                success: false,
+                message: "Password and Confirm Password Does Not Match, Try Again"
+            })
+        }
+        // hash
+        const hashedPassword = await bcrypt.hash(password,10);
+        //create entry in db
+        const profile = await Profile.create({
+            age: null,
+            bloodGroup: null,
+            gender: null,
+            disease: null,
+            exercise: null,
+            height: null,
+            weight: null
+        })
+        const result = await User.create({
+            firstName,
+            lastName,
+            email,
+            phoneNumber,
+            password: hashedPassword,
+            additionalData: profile._id
+        })
+
+        return res.status(200).json({
+            success: true,
+            message: "User Registered Successfully!"
+        })
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Error Occurred While registering, Please Try Again"
+        })
+    }
+}
 //Login
+const login = async (req,res) => {
+    try{
+        //fetch data - email and password
+        const {email,password} = req.body;
+        //data validation
+        if(!email || !password){
+            return res.status(401).json({
+                success: false,
+                message: "All fields are required"
+            })
+        }
+        //user Exists?
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.status(404).json({
+                success: false,
+                message: "User Not Found"
+            })
+        }
+        // password match with password present in db - await bcrypt.compare(password,user.password)
+        const isMatch = await bcrypt.compare(password,user.password);
+
+        if(!isMatch){
+            return res.status(403).json({
+                success: false,
+                message: "Incorrect Password"
+            })
+        }
+
+        const payload = {
+            email,
+            exp: Date.now() + 5 * 24 * 60 * 60 * 1000
+        }
+        const token = jwt.sign(payload,process.env.JWT_SECRET);
+
+        if(!token){
+            return res.status(403).json({
+                success: false,
+                message: "Failed To Generate Token"
+            })
+        }
+
+        const options = {
+            expiresIn: Date.now() + 5 * 24 * 60 * 60 * 1000,
+            httpOnly: true
+        }
+
+        res.cookie("token",token,options).status(200).json({
+            success: true,
+            message: "User Logged In Successfully",
+            token,
+            user
+        })
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Error Occurred While Logging In, Please Try Again"
+        })
+    }
+}
 
 //changePassword
+
+// const result = await User.findOneAndUpdate({email},{
+//     password: hashedPassword
+// },{new: true})
